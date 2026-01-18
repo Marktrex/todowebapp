314f71fe74f2f12cdf7a81f9983e8460c738003b
Marc <sinicchi123@gmail.com>
2026-01-18 23:33:35 +0800

    Update Version 1.3

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const inputType = document.getElementById("inputType");
 const enterBtn = document.getElementById("enterBtn");
 const todoLists = document.getElementById("todoLists");
 
-//Enter button function
+//Enter button function that adds data in the lists array
 enterBtn.addEventListener("click", function(){
     let text = inputType.value; //i am putting the input type value in a variable text
 
@@ -20,8 +20,28 @@ enterBtn.addEventListener("click", function(){
 
     lists.push(text); //this will push or put the data in the lists array
     
-    todoLists.innerHTML = lists.join("<br>");
-
     inputType.value = "" //this code is to clear the input type after adding something in the todo list
+
+    renderList(); //we will call the render function
 });
 
+//function to render/show the data in the list
+function renderList(){
+    todoLists.innerHTML = ""; //clears old list
+
+    lists.forEach(function(item, index){ //for loop where itme is the current todo text and index is the position
+        const li = document.createElement("li");  //it will put the text inside
+        li.textContent = item;
+
+        li.addEventListener("click",function(){ //click to delete, it calls the function remove item when clicked
+            removeItem(index);
+        });
+
+        todoLists.appendChild(li);
+    })
+}
+
+function removeItem(index){
+    lists.splice(index, 1);
+    renderList();
+}
