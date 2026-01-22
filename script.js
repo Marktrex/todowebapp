87dafe3b4b4350bb9c05e2a2247637c6bc35463d
Marc <sinicchi123@gmail.com>
2026-01-22 20:30:10 +0800

    Updated Version 1.4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /*
 System flow
 - user type todo + click the add button to trigger the function
@@ -7,12 +8,13 @@ System flow
 and refresh the list
 */
 let todo = [];
-let completedtodo = []
+let completedTodo = [];
 
 const inputType = document.getElementById("inputType");
 const enterBtn = document.getElementById("enterBtn");
 const todoLists = document.getElementById("todoLists");
 const completedLists = document.getElementById("completedLists");
+
 //Enter button function that adds data in the lists array
 enterBtn.addEventListener("click", function(){
     let text = inputType.value; //i am putting the input type value in a variable text
@@ -29,17 +31,34 @@ enterBtn.addEventListener("click", function(){
 //function to render/show the data in the list
 function renderList(){
     todoLists.innerHTML = ""; //clears old list
+    completedLists.innerHTML = "";
 
     todo.forEach(function(item, index){ //for loop where itme is the current todo text and index is the position
         const li = document.createElement("li");  //it will put the text inside
         li.textContent = item;
 
-        li.addEventListener("click",function(){ //click to delete, it calls the function remove item when clicked
-            removeItem(index);
+        li.addEventListener("click",function(){ //click to MOVE, it calls the function MOVE the item when clicked
+            moveToCompleted(index);
         });
 
         todoLists.appendChild(li);
-    })
+    });
+
+    //to render COMPLETED list
+    completedTodo.forEach(function (item) {
+        const li = document.createElement("li");
+        li.textContent = item;
+        completedLists.appendChild(li);
+    });
+
+}
+
+// Move todo to completed
+function moveToCompleted(index) {
+    const item = todo[index];   // get the item
+    todo.splice(index, 1);      // remove from todo
+    completedTodo.push(item);   // add to completed
+    renderList();
 }
 
 // function removeItem(index){
